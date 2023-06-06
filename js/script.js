8a0911c8c519dd6e6f2089acc3525316c4ae0b9e
Gino804 <giacomo.p03@gmail.com>
2023-06-06 16:03:27 +0200

    Added palindrome exercise

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log('JS OK');
+
+// Parola palindroma
+
+// Creo una funzione che, data una parola, mi faccia sapere se è palindroma
+function isPalindrome(word)
+{
+    let wordReverse = "";
+    let j = word.length;
+
+    // Creo la parola al contrario
+    for(let i = 0; i < word.length; i++)
+    {
+        wordReverse += word.substring(j - 1, j);
+        j--;
+    }
+
+    // Verifico se la parola è palindroma o no
+    if(wordReverse === word) return true;
+    else return false;
+}
+
+const userWord = prompt('Inserisci una parola: ');
+
+if(isPalindrome(userWord) === true) console.log("La parola è palindroma");
+else console.log("La parola non è palindroma");
